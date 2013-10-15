b5823c14a20e5f1b7f8653b604b1c16c5d2b4d41
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-10-15 14:54:34 -0400

    Adds Bootstrap support.

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,17 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ngRoute',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
+  'appControllers'
+]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'Accordion'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
