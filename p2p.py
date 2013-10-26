d26db74b3b1f29a9288ef2803ffb67fea1cb40c3
Jorge Villasmil <jvillasmil3@gmail.com>
2013-10-25 20:47:17 -0400

    Fixed pymongo connection import error

--- p2p.py
+++ p2p.py
@@ -18,7 +18,7 @@ MONGO_URL = os.environ.get('MONGOHQ_URL')
  
 if MONGO_URL:
     # Get a connection
-    conn = pymongo.Connection(MONGO_URL)
+    conn = Connection(MONGO_URL)
     
     # Get the database
     db = conn[urlparse(MONGO_URL).path[1:]]
