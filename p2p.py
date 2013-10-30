a0e2691ab73281590c272c16380fb9dc61605565
Jorge Villasmil <jvillasmil3@gmail.com>
2013-10-30 19:08:51 -0400

    Heroku Fix #2

--- p2p.py
+++ p2p.py
@@ -115,4 +115,4 @@ def add_question():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
